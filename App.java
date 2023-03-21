92345888c4af152cfd0e5522dc1b51cb2b8f8eb8
Zina BREXEL <121940282+ZinaBrexel@users.noreply.github.com>
2023-03-21 16:01:40 +0100

    Ajout d'un boucle Do while pour redemander le choix à l'utilisateur

--- App.java
+++ App.java
@@ -10,37 +10,38 @@ public class App {
         Breton vBreton = new Breton();
         vBreton.addDictionary("Bonjour", "Demat");
 
-        // Afficher les langues disponibles
+// Afficher les langues disponibles
         System.out.println("Langues disponibles : ");
         for (MyLanguage language : MyLanguage.values()) {
             System.out.println(language.name() + " - " + language.getDisplayName());
         }
 
-        // Demander à l'utilisateur de choisir une langue
-        System.out.println("Dans quelle langue souhaitez-vous dire 'Bonjour' ?");
-        String input = scan.nextLine();
-
-
-        // Vérifier que la langue saisie est valide
         MyLanguage selectedLanguage = null;
-        try {
-            selectedLanguage = MyLanguage.valueOf(input.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Langue invalide.");
-        }
+        boolean isValidLanguage = false;
+        do {
+            // Demander à l'utilisateur de choisir une langue
+            System.out.println("Dans quelle langue souhaitez-vous dire 'Bonjour' ?");
+            String input = scan.nextLine();
 
-        // Utiliser la langue sélectionnée
-        if (selectedLanguage != null) {
-            switch (selectedLanguage) {
-                case LANGUAGE1:
-                    System.out.println(vEnglish.getTraduction("Bonjour"));
-                    break;
-                case LANGUAGE2:
-                    System.out.println(vBreton.getTraduction("Bonjour"));
-                    break;
-                default:
-                    break;
+            // Vérifier que la langue saisie est valide
+            try {
+                selectedLanguage = MyLanguage.valueOf(input.toUpperCase());
+                isValidLanguage = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println("Langue invalide.");
             }
+        } while (!isValidLanguage);
+
+// Utiliser la langue sélectionnée
+        switch (selectedLanguage) {
+            case ANGLAIS:
+                System.out.println(vEnglish.getTraduction("Bonjour"));
+                break;
+            case BRETON:
+                System.out.println(vBreton.getTraduction("Bonjour"));
+                break;
+            default:
+                break;
         }
     }
 }
